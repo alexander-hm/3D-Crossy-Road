9ada325e9afe2e8d29337a86589757c3a3c47236
alexander-hm <alexanderhuang5@gmail.com>
2022-02-11 10:37:08 -0500

    Road crossing game in 3D.

---
+++ Assets/Code/Movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public Animator anim;
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animations
+        if (Input.GetKeyDown("w") || Input.GetKeyDown("a") || Input.GetKeyDown("s") || Input.GetKeyDown("d"))
+        {
+            anim.SetTrigger("Walk");
+        }
+        if (!Input.anyKey)
+        {
+            anim.SetTrigger("Idle");
+        }
+        //movements
+        if (Input.GetKey("w"))
+        {
+            //rb.AddForce(0, 0, 1500f * Time.deltaTime);
+            transform.position += new Vector3(0, 0, 0.02f);
+        }
+        if (Input.GetKey("a"))
+        {
+            //rb.AddForce(-500f * Time.deltaTime, 0, 0);
+            transform.position += new Vector3(-0.01f, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            //rb.AddForce(0, 0, -1000f * Time.deltaTime);
+            transform.position += new Vector3(0, 0, -0.01f);
+        }
+        if (Input.GetKey("d"))
+        {
+            //rb.AddForce(500f * Time.deltaTime, 0, 0);
+            transform.position += new Vector3(0.01f, 0, 0);
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            //rb.AddForce(500f * Time.deltaTime, 0, 0);
+            transform.position += new Vector3(0, 0.5f, 0);
+            transform.position += new Vector3(0, 0.5f, 0);
+        }
+    }
+}
---
+++ Assets/Code/arrow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+    bool up;
+    public int speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        up = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (up)
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        else
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Directional")
+        {
+            up = !up;
+        }
+    }
+}
